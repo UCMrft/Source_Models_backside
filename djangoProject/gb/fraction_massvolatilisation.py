998faefd50dcb55b0f3225196332f247fbddb0bb
UCMrft <109007398+UCMrft@users.noreply.github.com>
2023-09-12 19:43:26 +0800

    Update fraction_massvolatilisation.py

--- djangoProject/gb/fraction_massvolatilisation.py
+++ djangoProject/gb/fraction_massvolatilisation.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def fraction_massvolatilisation(request):
     body = json.loads(request.body)
+    name = body.get('name')
     m_w = body.get('m_w')
     k = body.get('k')
     p_sat = body.get('p_sat')
@@ -16,7 +17,8 @@ def fraction_massvolatilisation(request):
     q_m = m_w * k * p_sat * 1000 / (t_l * 8.314)
     res = {
         'code': 1,
-        'data': q_m,
+        'data1': name,
+        'data2': q_m,
         'msg': '运算成功'
     }
     return JsonResponse(res)
